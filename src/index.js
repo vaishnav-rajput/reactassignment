299bfa6b1b459b044cdec01e478bf56ffdf567d4
vaishnav-rajput <vaishnav.rajput108@gmail.com>
2024-10-05 16:39:32 +0530

    file structure

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+    <>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
+    <Toaster/>
+    </BrowserRouter>
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
